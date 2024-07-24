1baee4353ff6bf5df23cb46a501d763ce5545a32
Diego Francisco Rosales Canalejo <drosales@c3r5s4.42malaga.com>
2024-07-24 11:28:48 +0200

    In progress

--- push-swap/ft_push.c
+++ push-swap/ft_push.c
@@ -6,7 +6,7 @@
 /*   By: drosales <drosales@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/24 08:12:39 by drosales          #+#    #+#             */
-/*   Updated: 2024/07/24 09:17:06 by drosales         ###   ########.fr       */
+/*   Updated: 2024/07/24 11:28:09 by drosales         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,13 +24,13 @@ static void    ft_push(t_stack **dst, t_stack **src)
     *src = tmp;
 }
 
-static void    pa(t_stack **stack_a, t_stack **stack_b)
+void    pa(t_stack **stack_a, t_stack **stack_b)
 {
     ft_push(*stack_a, *stack_b);
     write(1, "pa\n", 3);
 }
 
-static void    pb(t_stack **stack_a, t_stack **stack_b)
+void    pb(t_stack **stack_a, t_stack **stack_b)
 {
     ft_push(*stack_b, *stack_a);
     write(1, "pb\n", 3);
