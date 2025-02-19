19188bab7287c4eb5306761271efe240ede42301
drosales <drosales@student.42malaga.com>
2025-02-19 12:37:00 +0100

    EX02 in progress

---
+++ CPP/Module_00/ex02/src/Init.cpp
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Init.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: drosales <drosales@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/19 12:30:13 by drosales          #+#    #+#             */
+/*   Updated: 2025/02/19 12:36:43 by drosales         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/Account.hpp"
+
+// Initialation of global var
+
+int Account::_nbAccounts = 0;
+int Account::_totalAmount = 0;
+int Account::_totalNbDeposits = 0;
+int Account::_totalNbWithdrawals = 0;
+
